890cbf56bd1f8a9d52dd689e6278527a6796f5d1
Samrat Ghosh <samratat@gmail.com>
2021-08-09 00:48:36 +0530

    Added Icon, Fixed Radio Button Related issues

--- components/header/header.js
+++ components/header/header.js
@@ -1,19 +1,33 @@
+// Next
+import Image from "next/image";
+
 const Header = () => {
-    return (
-        <article>
-            <header className="app__header">
-                <div className="app__header__contents">
-                    <div className="app__header__contents__left">
-                        <p className="app__header__contents__left__text">Finding Falcone</p>
-                    </div>
-                    <div className="app__header__contents__right">
-                        <button className="app__header__contents__right__cta app__header__contents__right__cta--first">Planets</button>
-                        <button className="app__header__contents__right__cta app__header__contents__right__cta--second">Vehicles</button>
-                    </div>
-                </div>
-            </header>
-        </article>
-    )
+  return (
+    <article>
+      <header className="app__header">
+        <div className="app__header__contents">
+          <div className="app__header__contents__left">
+            <Image
+              alt="Falcons Icon"
+              src="/assets/falcons.png"
+              width={32}
+              height={32}
+              layout="fixed"
+            />
+            <p className="app__header__contents__left__text">Finding Falcone</p>
+          </div>
+          <div className="app__header__contents__right">
+            <button className="app__header__contents__right__cta app__header__contents__right__cta--first">
+              Planets
+            </button>
+            <button className="app__header__contents__right__cta app__header__contents__right__cta--second">
+              Vehicles
+            </button>
+          </div>
+        </div>
+      </header>
+    </article>
+  );
 };
 
 export default Header;
