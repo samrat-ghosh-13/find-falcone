af2d73d817294f7e33699d3ca28b3c90eb175cdc
Samrat Ghosh <samratat@gmail.com>
2021-08-25 03:14:02 +0530

    Added Test Cases for Button

--- components/button/button.spec.js
+++ components/button/button.spec.js
@@ -12,11 +12,11 @@ import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 /**
- * @name TestComponent
+ * @name ButtonWrapper
  * @details use case of button component
  * @returns button component with the updated count
  */
-const TestComponent = () => {
+const ButtonWrapper = () => {
   const [count, setCounter] = useState(0);
 
   const handleButtonClick = () => {
@@ -28,54 +28,47 @@ const TestComponent = () => {
   );
 };
 
-// Case 1: Button Renders properly or not
-it("Case 1: Button Component Renders Correctly", () => {
-  const handleButtonClick = () => {
-    console.log("Button Clicked");
-  };
+describe("Test Button component", () => {
+  // Case 1: Button Renders properly or not using snapshot testing
+  it("Case 1: Button Component Renders Correctly", () => {
+    const handleButtonClick = () => {
+      console.log("Button Clicked");
+    };
 
-  const buttonComponentTree = renderer
-    .create(<Button handleClick={handleButtonClick}>Test</Button>)
-    .toJSON();
-  expect(buttonComponentTree).toMatchSnapshot();
-});
+    const buttonComponentTree = renderer
+      .create(
+        <ButtonWrapper handleClick={handleButtonClick}>Test</ButtonWrapper>
+      )
+      .toJSON();
+    expect(buttonComponentTree).toMatchSnapshot();
+  });
 
-// Case 2: Checks the Button data-testid is correct or not
-it("Case 2: Checks the Button data-testId is correct or not", () => {
-  const handleButtonClick = () => {
-    console.log("Button Clicked");
-  };
-  const { getByTestId } = render(
-    <Button handleClick={handleButtonClick}>Test</Button>
-  );
-  expect(getByTestId("button")).toBeInTheDocument();
-});
+  // Case 2: Checks the Button data-testid is correct or not
+  it("Case 2: Checks the Button data-testId is correct or not", () => {
+    const handleButtonClick = () => {
+      console.log("Button Clicked");
+    };
+    const { getByTestId } = render(
+      <ButtonWrapper handleClick={handleButtonClick}>Test</ButtonWrapper>
+    );
+    expect(getByTestId("button")).toBeInTheDocument();
+  });
 
-// Case 3: Checks the Button data-testid is correct or not
-it("Case 3: Checks the Button Text data-testId is correct or not", () => {
-  const handleButtonClick = () => {
-    console.log("Button Clicked");
-  };
-  const { getByTestId } = render(
-    <Button handleClick={handleButtonClick}>Test</Button>
-  );
-  expect(getByTestId("button")).toBeInTheDocument();
-});
+  // Case 3: Checks the number of times button is clicked
+  it("Case 3: Checks the number of times button is clicked", () => {
+    const { getByTestId, getByText } = render(<ButtonWrapper />);
+    fireEvent.click(getByTestId("button"));
+    fireEvent.click(getByTestId("button"));
+    expect(
+      getByText("Click to increase: 2", { exact: true })
+    ).toBeInTheDocument();
+  });
 
-// Case 4: Checks the number of times button is clicked
-it("Case 4: Checks the number of times button is clicked", () => {
-  const { getByTestId, getByText } = render(<TestComponent />);
-  fireEvent.click(getByTestId("button"));
-  fireEvent.click(getByTestId("button"));
-  expect(
-    getByText("Click to increase: 2", { exact: true })
-  ).toBeInTheDocument();
-});
-
-// Case 5: Checks the Button Component Name is correct or Not
-it("Case 5: Checks the Button Component Name is correct or Not", () => {
-  const { getByText } = render(<TestComponent />);
-  expect(
-    getByText("Click to increase: 0", { exact: true })
-  ).toBeInTheDocument();
+  // Case 4: Checks the Button Component Name is correct or Not
+  it("Case 4: Checks the Button Component Children is correct or Not", () => {
+    const { getByText } = render(<ButtonWrapper />);
+    expect(
+      getByText("Click to increase: 0", { exact: true })
+    ).toBeInTheDocument();
+  });
 });
